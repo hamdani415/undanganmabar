7ccc1c2ea1949d2387bd32c74b73085f58865874
hamdani415 <neswakun27@gmail.com>
2024-09-28 14:58:00 +0700

    Update hamdani.js

--- hamdani.js
+++ hamdani.js
@@ -79,8 +79,8 @@ function iya () {
 simplyCountdown ('.simply-countdown' , {
   year: 2024, // required
   month: 9, // required
-  day: 22, // required
-  hours: 20, // Default is 0 [0-23] integer
+  day: 28, // required
+  hours: 21, // Default is 0 [0-23] integer
   minutes: 0, // Default is 0 [0-59] integer
   seconds: 0, // Default is 0 [0-59] integer
   words: { //words displayed into the countdown
